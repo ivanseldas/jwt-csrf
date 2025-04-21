d76d244fe6c3bb2183f2a580c191333a72649cc3
ivanseldas <ivanseldasp@gmail.com>
2025-04-21 02:12:25 +0200

    eliminar archivos .py de carpeta root

--- main.py
+++
@@ -1,9 +0,0 @@
-from fastapi import FastAPI
-from auth_router import router 
-
-app = FastAPI()
-app.include_router(router)
-
-@app.get("/")
-async def root():
-    return {"JWT Authentication Test"}
